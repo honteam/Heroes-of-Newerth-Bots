03870138c870761485513cc97027809726b85349
Patrick Cockwell <pcockwell@gmail.com>
2013-09-17 15:31:29 -0400

    more dev -> community

--- bots/engineer/engi_main.lua
+++ bots/engineer/engi_main.lua
@@ -392,7 +392,6 @@ local function funcFindItemsOverride(botBrain)
 end
 object.FindItemsOld = core.FindItems
 core.FindItems = funcFindItemsOverride
-
  
 ----------------------------------
 --	EngiBot's Retreating Tactics
