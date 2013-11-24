5f9c29691a7389394fcde714c1458efe4f5050bd
Kairus <kerey14@hotmail.com>
2013-11-24 14:01:42 +1300

    Performance increase to core.getitem when the bot doesn't yet have the item. Fixed a bug related to ValidateItem still not making the item nil if invalid.

--- bots/core.lua
+++ bots/core.lua
@@ -1182,27 +1182,34 @@ function core.InventoryContains(inventory, val, bIgnoreRecipes, bIncludeStash)
 end
 
 core.tFoundItems = {}
+core.tsearchTimes = {}
 --Finds an item on your hero.
 function core.GetItem(val, bIncludeStash)
 	if core.tFoundItems[val] then -- We have checked the item before. Validate it.
 		core.ValidateItem(core.tFoundItems[val])
-		if core.tFoundItems[val] then -- still valid, return it
+		if core.tFoundItems[val] and core.tFoundItems[val]:IsValid() then -- still valid, return it
 			return core.tFoundItems[val]
+		else
+			core.tFoundItems[val] = nil
 		end
 	end
-
 	--First time seeing the item, or it was invalidated.
 	if not core.tFoundItems[val] then
-		inventory = core.unitSelf:GetInventory()
-		if bIncludeStash == nil then
-			bIncludeStash = false
-		end
-		local nLast = (bIncludeStash and 12) or 6
-		for slot = 1, nLast, 1 do
-			local curItem = inventory[slot]
-			if curItem then
-				if curItem:GetTypeName() == val and not curItem:IsRecipe() then --ignore recipes!
-					return core.WrapInTable(curItem)
+		local nLastSearchTime = core.tsearchTimes[val] or 0
+		if nLastSearchTime + 500 <= HoN:GetGameTime() then --only look at max 2 times a second
+			core.tsearchTimes[val] = HoN:GetGameTime()
+			inventory = core.unitSelf:GetInventory()
+			if bIncludeStash == nil then
+				bIncludeStash = false
+			end
+			local nLast = (bIncludeStash and 12) or 6
+			for slot = 1, nLast, 1 do
+				local curItem = inventory[slot]
+				if curItem then
+					if curItem:GetTypeName() == val and not curItem:IsRecipe() then --ignore recipes!
+						core.tFoundItems[val] = core.WrapInTable(curItem)
+						return core.tFoundItems[val]
+					end
 				end
 			end
 		end
