0254953d2c476a9d62edc58bd625e67caa821c15
Kairus <kerey14@hotmail.com>
2013-12-04 09:24:23 +1300

    changes for submission

--- bots/behaviorLib.lua
+++ bots/behaviorLib.lua
@@ -3055,7 +3055,7 @@ function behaviorLib.SellLowestItems(botBrain, numToSell)
 
 		if lowestItem then
 			BotEcho("Selling "..lowestItem:GetName().." in slot "..lowestItem:GetSlot())
-			behaviorLib.addItemBehavior(lowestItem:GetName(), true)
+			behaviorLib.removeItemBehavior(lowestItem:GetName())
 			core.unitSelf:Sell(lowestItem)
 			inventory[lowestItem:GetSlot()] = ""
 			numToSell = numToSell - 1
@@ -3279,7 +3279,7 @@ Current algorithm:
 		if #componentDefs == 0 then
 			behaviorLib.ShuffleCombine(botBrain, nextItemDef, unitSelf)
 		end
-		behaviorLib.addItemBehavior(nextItemDef:GetName(), false)
+		behaviorLib.addItemBehavior(nextItemDef:GetName())
 	end
 
 	bShuffled = behaviorLib.SortInventoryAndStash(botBrain)
