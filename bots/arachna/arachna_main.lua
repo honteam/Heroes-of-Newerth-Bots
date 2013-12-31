ac25fb64ec1af20135e27f6abeb1f0bd70090c4c
Michael Tesija <mtesija@umich.edu>
2013-12-30 19:37:54 -0500

    Update arachna_main.lua

--- bots/arachna/arachna_main.lua
+++ bots/arachna/arachna_main.lua
@@ -37,7 +37,6 @@ runfile "bots/botbraincore.lua"
 runfile "bots/eventsLib.lua"
 runfile "bots/metadata.lua"
 runfile "bots/behaviorLib.lua"
-runfile "bots/illusionLib.lua"
 
 local core, eventsLib, behaviorLib, metadata, skills = object.core, object.eventsLib, object.behaviorLib, object.metadata, object.skills
 
