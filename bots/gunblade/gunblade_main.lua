94b1922c9d52b20e6732e14c90c362dbcf3b3a28
ModernSaint <alexPazder@hotmail.ca>
2014-12-04 13:26:17 -0800

    Gunblade Bot Submission
    A bot submission for Gunblade.
    Forum Post:
    http://forums.heroesofnewerth.com/showthread.php?550581-Gunblade-Submission&p=16211596#post16211596

---
+++ bots/gunblade/gunblade_main.lua
@@ -0,0 +1,390 @@
+-----------------------------------------------------------------------------
+--	 _________                   __        _                 __  
+--	/  _____  |                 |  |      | |               |  |  
+--	| /     |_|                 |  |      | |               |  |   
+--	| |          _   _   _      |  |___   | |   ____     ___|  |   ___
+--	| |    ____ | | | | | |___  |   _  \  | |  / _  \   /  _   |  / _ \
+--	|  \___\  / | \_/ | |  _  | |  |_|  | | | | /_|  \ |  |_|  | |  __/
+--	\________/   \___/  |_| |_|  \_____/  |_|  \___/\_\ \______/  \___|
+------------------------------------------------------------------------------
+--V 1.0
+--Coded By: ModernSaint
+--Basic Equations--
+local _G = getfenv(0)
+local object = _G.object
+object.myName = object:GetName()
+
+object.bRunLogic   	= true
+object.bRunBehaviors	= true
+object.bUpdates 		= true
+object.bUseShop 		= true
+object.bRunCommands 	= true
+object.bMoveCommands 	= true
+object.bAttackCommands 	= true
+object.bAbilityCommands = true
+object.bOtherCommands 	= true
+object.bReportBehavior = false
+object.bDebugUtility = false
+object.bDebugExecute = false
+object.logger = {}
+object.logger.bWriteLog = false
+object.logger.bVerboseLog = false
+object.core 		= {}
+object.eventsLib 	= {}
+object.metadata 	= {}
+object.behaviorLib 	= {}
+object.skills 		= {}
+runfile "bots/core.lua"
+runfile "bots/botbraincore.lua"
+runfile "bots/eventsLib.lua"
+runfile "bots/metadata.lua"
+runfile "bots/behaviorLib.lua"
+local core, eventsLib, behaviorLib, metadata, skills = object.core, object.eventsLib, object.behaviorLib, object.metadata, object.skills
+
+local print, ipairs, pairs, string, table, next, type, tinsert, tremove, tsort, format, tostring, tonumber, strfind, strsub
+	= _G.print, _G.ipairs, _G.pairs, _G.string, _G.table, _G.next, _G.type, _G.table.insert, _G.table.remove, _G.table.sort, _G.string.format, _G.tostring, _G.tonumber, _G.string.find, _G.string.sub
+local ceil, floor, pi, tan, atan, atan2, abs, cos, sin, acos, max, random
+	= _G.math.ceil, _G.math.floor, _G.math.pi, _G.math.tan, _G.math.atan, _G.math.atan2, _G.math.abs, _G.math.cos, _G.math.sin, _G.math.acos, _G.math.max, _G.math.random
+
+local BotEcho, VerboseLog, BotLog = core.BotEcho, core.VerboseLog, core.BotLog
+local Clamp = core.Clamp
+
+BotEcho('loading Gunbladebot...')
+
+--------------------------------
+-- Lanes
+--------------------------------
+
+core.tLanePreferences = {Jungle = 0, Mid = 5, ShortSolo = 3, LongSolo = 1, ShortSupport = 2, LongSupport = 2, ShortCarry = 4, LongCarry = 4}
+
+object.heroName = 'Hero_Gunblade'
+
+----------------------------------
+--	Item Build
+----------------------------------	
+
+behaviorLib.StartingItems = 
+  {"2 Item_RunesOfTheBlight", "Item_Soulscream"}
+behaviorLib.LaneItems = 
+  {"Item_Marchers", "Item_EnhancedMarchers"}
+behaviorLib.MidItems = 
+  { "Item_Energizer", "Item_Lightning2", "Item_Critical1 4", "Item_SolsBulwark"}
+behaviorLib.LateItems = 
+  { "Item_Evasion", "Item_DaemonicBreastplate", "Item_Weapon3"}
+
+--------------------------------
+-- Skills
+--------------------------------
+
+local bSkillsValid = false
+function object:SkillBuild()
+
+	local unitSelf = self.core.unitSelf
+
+	if not bSkillsValid then
+		skills.CripplingSlugs = unitSelf:GetAbility(0)
+		skills.DemonicShield = unitSelf:GetAbility(1)
+		skills.LethalRange = unitSelf:GetAbility(2)
+		skills.GrapplingShot = unitSelf:GetAbility(3)
+		skills.attributeBoost = unitSelf:GetAbility(4)
+		
+		if skills.CripplingSlugs and skills.DemonicShield and skills.GrapplingShot and skills.attributeBoost then
+			bSkillsValid = true
+		else
+			return
+		end
+	end
+		
+	if unitSelf:GetAbilityPointsAvailable() <= 0 then
+		return
+	end
+	
+	--Leveling order:
+	if not (skills.CripplingSlugs:GetLevel() >= 1) then --CS to lvl 1
+		skills.CripplingSlugs:LevelUp()
+	elseif not (skills.DemonicShield:GetLevel() >= 1) then --DS to lvl 1
+		skills.DemonicShield:LevelUp()
+	elseif not (skills.LethalRange:GetLevel() >= 3) then --LR to lvl 2
+		skills.LethalRange:LevelUp()
+	--Will maximize ability is in this order.
+	elseif skills.GrapplingShot:CanLevelUp() then
+		skills.GrapplingShot:LevelUp()
+	elseif skills.CripplingSlugs:CanLevelUp() then
+		skills.CripplingSlugs:LevelUp()
+	elseif skills.LethalRange:CanLevelUp() then
+		skills.LethalRange:LevelUp()
+	elseif skills.DemonicShield:CanLevelUp() then
+		skills.DemonicShield:LevelUp()
+	else
+		skills.attributeBoost:LevelUp()
+	end	
+end
+
+----------------------------------
+--	Harass Bonuses
+----------------------------------
+
+-- bonus agression points if a skill/item is available for use
+object.nCripplingSlugsUp = 14
+object.nDemonicShieldUp = 6
+object.nGrapplingShotUp = 20
+object.nEnergizerUp = 6
+
+local function AbilitiesUpUtilityFn()
+	local val = 0
+	
+	if skills.CripplingSlugs:CanActivate() then
+		val = val + object.nCripplingSlugsUpBonus
+	end
+	
+		if skills.DemonicShield:CanActivate() then
+		val = val + object.nDemonicShieldUpBonus
+	end
+	
+	if skills.GrapplingShot:CanActivate() then
+		val = val + object.nGrapplingShotUpBonus
+	end
+	
+	if core.itemEnergizer and core.itemEnergizer:CanActivate() then
+		val = val + object.nEnergizerUp
+	end
+	
+	return val
+end
+
+-- bonus agression points that are applied to the bot upon successfully using a skill/item
+object.nCripplingSlugsUse = 24
+object.nDemonicShieldUse = 8
+object.nGrapplingShotUse = 40
+object.nEnergizerUse = 13
+
+function object:oncombateventOverride(EventData)
+	self:oncombateventOld(EventData)
+	
+	local addBonus = 0
+	
+	if EventData.Type == "Ability" then	
+		if EventData.InflictorName == "Ability_Gunblade1" then
+			addBonus = addBonus + object.nCripplingSlugsUseBonus
+		end
+		if EventData.InflictorName == "Ability_Gunblade2" then
+			addBonus = addBonus + object.nDemonicShieldUseBonus
+		end
+		if EventData.InflictorName == "Ability_Gunblade4" then
+			addBonus = addBonus + object.nGrapplingShotUseBonus
+		end
+	
+	elseif EventData.Type == "Item" then
+		if core.itemEnergizer ~= nil and EventData.SourceUnit == core.unitSelf:GetUniqueID() and EventData.InflictorName == core.itemEnergizer:GetName() then
+			addBonus = addBonus + self.nEnergizerUse
+		end
+	end
+
+	
+	if addBonus > 0 then
+		--decay before we add
+		core.DecayBonus(self)
+	
+		core.nHarassBonus = core.nHarassBonus + addBonus
+	end
+end
+
+object.oncombateventOld = object.oncombatevent
+object.oncombatevent 	= object.oncombateventOverride
+
+-- thresholds of aggression the bot must reach to use these abilities
+object.nCripplingSlugsThreshold = 20
+object.nDemonicShieldThreshold = 18
+object.nGrapplingShotThreshold = 24
+object.nEnergizerThreshold = 12
+
+-- Additional Modifiers
+
+--weight overrides
+behaviorLib.nCreepPushbackMul = 1
+behaviorLib.nTargetPositioningMul = 1
+
+----------------------------------
+--	harass actions
+----------------------------------
+
+local function HarassHeroExecuteOverride(botBrain)
+	local bDebugEchos = false
+	
+	local unitTarget = behaviorLib.heroTarget
+	if unitTarget == nil or not unitTarget:IsValid() then
+		return false --can not execute, move on to the next behavior
+	end
+	
+	local unitSelf = core.unitSelf
+	
+	local nAttackRange = core.GetAbsoluteAttackRangeToUnit(unitSelf, unitTarget)
+	local nAttackRangeSq = nAttackRange * nAttackRange
+
+	local nMyExtraRange = core.GetExtraRange(unitSelf)
+	local vecMyPosition = unitSelf:GetPosition()	
+	local vecTargetPosition = unitTarget:GetPosition()
+	local nTargetExtraRange = core.GetExtraRange(unitTarget)
+	local nTargetDistanceSq = Vector3.Distance2DSq(vecMyPosition, vecTargetPosition)
+	
+	local nLastHarassUtility = behaviorLib.lastHarassUtil
+	
+	local bActionTaken = false
+	local abilDemonicShield = skills.DemonicShield
+	local abilCripplingSlugs = skills.CripplingSlugs
+	local abilGrapplingShot = skills.GrapplingShot
+	
+	--CripplingSlugs
+	if not bActionTaken and nLastHarassUtility > botBrain.nCripplingSlugsThreshold then
+		if abilCripplingSlugs:CanActivate() then
+			local nRange = abilCripplingSlugs:GetRange()
+			if nTargetDistanceSq < (nRange * nRange) and unitSelf:GetManaPercent() > .20 then
+				bActionTaken = core.OrderAbilityPosition(botBrain, abilCripplingSlugs, vecTargetPosition)
+			elseif nTargetDistanceSq < (nAttackRangeSq * .4) then --Use ability if an enemy is close
+				bActionTaken = core.OrderAbilityPosition(botBrain, CripplingSlugs, vecTargetPosition)		
+			end
+		end
+	end
+	
+	--DemonicShield
+	if not bActionTaken and abilDemonicShield:CanActivate() then
+		if nLastHarassUtility > botBrain.nDemonicShieldThreshold then --activate on utility calc
+			bActionTaken = core.OrderAbility(botBrain, abilDemonicShield)
+		elseif (core.NumberElements(tLocalEnemyHeroes) > 2) then --activate if more than 2 hostiles nearby
+			bActionTaken = core.OrderAbility(botBrain, abilDemonicShield)
+		elseif .45 > unitSelf:GetHealthPercent() and (core.NumberElements(tLocalEnemyHeroes) > 0) then --lower than 45% health with an enemy nearby
+			bActionTaken = core.OrderAbility(botBrain, abilDemonicShield)
+		end
+	end
+	
+	--GrapplingShot
+	if not bActionTaken and (nLastHarassUtility > botBrain.nGrapplingShotThreshold) and abilGrapplingShot:CanActivate() then
+			local nRange = abilGrapplingShot:GetRange()
+		if nTargetDistanceSq < (nRange * nRange) then
+			bActionTaken = core.OrderAbilityEntity(botBrain, abilGrapplingShot, unitTarget)
+		elseif nTargetDistanceSq > (nRange * nRange) then --if not in range get closer. Speed items will be activated as well as DS b/c chances are he will take damage.
+			local posNeeded = unitTarget:GetPosition()				
+			if itemGhostMarchers and itemGhostMarchers:CanActivate() then
+				bActionTaken = core.OrderItemClamp(botBrain, unitSelf, itemGhostMarchers) --activate GM
+			end
+			if itemEnergizer and itemEnergizer:CanActivate() then
+				bActionTaken = core.OrderItemClamp(botBrain, unitSelf, itemEnergizer) --activate energizer
+			end
+			if abilDemonicShield:CanActivate() then --activate DemonicShield
+					bActionTaken = core.OrderAbility(botBrain, abilDemonicShield)
+			end
+			if not bActionTaken and behaviorLib.lastHarassUtil < behaviorLib.diveThreshold then --Check willingness to dive towers
+				desiredPos = core.AdjustMovementForTowerLogic(desiredPos)
+			end
+				core.OrderMoveToPosClamp(botBrain, unitSelf, desiredPos, false) --Move order to close in
+				bActionTaken = true
+		end
+	end
+	
+	
+	--Energizer
+	if not bActionTaken and core.itemEnergizer and core.itemEnergizer:CanActivate() and nHarassUtility > object.nEnergizerThreshold then
+		botBrain:OrderItem(core.itemEnergizer.object or core.itemEnergizer, false)
+	end
+	
+	if not bActionTaken then
+		if bDebugEchos then BotEcho("  No action yet, proceeding with normal harass execute.") end
+		return object.harassExecuteOld(botBrain)
+	end
+end
+object.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
+
+----------------------------------------
+--  	Retreat Behaviour   	  --
+----------------------------------------
+
+local function retreatFromThreatExecuteOverride(botBrain)
+	local abilDemonicShield = skills.DemonicShield
+	--Energizer use
+	if itemEnergizer and itemEnergizer:CanActivate() and behaviorLib.lastRetreatUtil >= object.nEnergizerThreshold then
+		botBrain:OrderItem(core.itemEnergizer.object or core.itemEnergizer, false)
+		object.nTimeEnergizered = HoN:GetGameTime()
+	end
+	--Use demonicSheild when fleeing
+	if (abilDemonicShield:CanActivate() and behaviorLib.lastRetreatUtil >= object.nDemonicShieldThreshold) then
+		core.OrderAbility(botBrain, abilDemonicShield, false, true)
+	end
+	--Activate GM when fleeing
+	if (core.itemGhostMarchers and core.itemGhostMarchers:CanActivate())then
+		botBrain:OrderItem(core.itemGhostMarchers.object or core.itemGhostMarchers, false)
+	end
+	   
+	return object.RetreatFromThreatExecuteOld(botBrain)
+end
+object.RetreatFromThreatExecuteOld = behaviorLib.RetreatFromThreatBehavior["Execute"]
+behaviorLib.RetreatFromThreatBehavior["Execute"] = retreatFromThreatExecuteOverride
+
+----------------------------------------
+--- Extras
+----------------------------------------
+
+--[[ colors:
+	red
+	aqua == cyan
+	gray
+	navy
+	teal
+	blue
+	lime
+	black
+	brown
+	green
+	olive
+	white
+	silver
+	purple
+	maroon
+	yellow
+	orange
+	fuchsia == magenta
+	invisible
+--]]
+
+----------------------------------------------------
+--  	   Heal At Well Override		  --
+----------------------------------------------------
+
+--return to well more often. --2000 gold adds 8 to return utility, 0 % mana also adds 8.
+--When returning to well, use skills and items.
+--Modifed from kairus101's BalphBot!
+local function HealAtWellUtilityOverride(botBrain)
+	local vecBackupPos = core.allyWell and core.allyWell:GetPosition() or behaviorLib.PositionSelfBackUp()
+	local nOldHealingMultiplier = 1.75
+	local nGoldSpendingDesire = 8 / 2000
+	local nManaRefillingDesire = 6 --0 if full, this if empty
+	
+	if (Vector3.Distance2DSq(core.unitSelf:GetPosition(), vecBackupPos) < 400 * 400 and core.unitSelf:GetManaPercent() * 100 < 95) then
+		return 80
+	end
+	return object.HealAtWellUtilityOld(botBrain) * nOldHealingMultiplier + (botBrain:GetGold() * nGoldSpendingDesire) + 1-(core.unitSelf:GetManaPercent()) * nManaRefillingDesire --courageously flee back to base.
+end
+local function HealAtWellExecuteOverride(botBrain)
+	local vecWellPos = (core.allyWell and core.allyWell:GetPosition()) or behaviorLib.PositionSelfBackUp()
+	local abilDemonicShield = skills.DemonicShield
+	
+	if (Vector3.Distance2DSq(core.unitSelf:GetPosition(), vecWellPos) > 600 * 600) then
+		if (core.itemEnergizer and core.itemEnergizer:CanActivate() and not unitSelf:HasState("State_Energizer_Buff"))then --when heading to base, use energizer
+			botBrain:OrderItem(core.itemEnergizer.object or core.itemEnergizer, false)
+			object.nTimeEnergizered = HoN:GetGameTime()
+		end
+		if (core.itemGhostMarchers and core.itemGhostMarchers:CanActivate())then --when heading to base, use boots
+			botBrain:OrderItem(core.itemGhostMarchers.object or core.itemGhostMarchers, false)
+		end
+		if abilDemonicShield:CanActivate() then --activate shield when heading back
+			core.OrderAbility(botBrain, abilDemonicShield)
+		end
+	end
+	return object.HealAtWellExecuteOld(botBrain)
+end
+object.HealAtWellUtilityOld = behaviorLib.HealAtWellBehavior["Utility"]
+object.HealAtWellExecuteOld = behaviorLib.HealAtWellBehavior["Execute"]
+behaviorLib.HealAtWellBehavior["Utility"] = HealAtWellUtilityOverride
+behaviorLib.HealAtWellBehavior["Execute"] = HealAtWellExecuteOverride
+
+BotEcho('finished loading Gunblade_main')
